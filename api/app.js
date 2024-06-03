692c8e6eab6d821baf0472b162deeca8d5301c50
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-03 22:48:34 +0300

    Create test api route.

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import "./db/index.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
