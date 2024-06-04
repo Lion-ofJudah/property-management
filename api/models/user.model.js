525f1cf60f22925465a997f4c0217edc7abf9063
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-04 16:38:57 +0300

    Implement hash password storing functionality.

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -18,6 +19,15 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const saltRound = 10;
+    const hashedPassword = await bcrypt.hash(this.password, saltRound);
+    this.password = hashedPassword;
+  }
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
