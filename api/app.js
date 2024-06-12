82e1eef44788a65a347d615453b3efe751fb6084
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-12 05:02:31 +0300

    Create listing api route.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import "./db/index.js";
 
@@ -11,6 +12,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
