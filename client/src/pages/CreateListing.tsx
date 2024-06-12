29363101f300d96a43bc0d72ced63a8158383691
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-12 16:17:05 +0300

    Implement property images upload functionality.

--- client/src/pages/CreateListing.tsx
+++ client/src/pages/CreateListing.tsx
@@ -1,3 +1,11 @@
+import { useState } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
 import Checkbox from "../components/Checkbox";
 import RippleButton from "../components/RippleButton";
 
@@ -6,6 +14,97 @@ interface Props {
 }
 
 export default function CreateListing({ currentUser }: Props) {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [uploading, setUploading] = useState(false);
+  const [imageUploadError, setImageUploadError] = useState({
+    status: false,
+    message: "",
+  });
+
+  console.log(formData);
+
+  const handleChange = (event: any) => {
+    setFiles(event.target.files);
+  };
+
+  const handleFileUpload = async (file: File) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            (downloadURL: string) => {
+              resolve(downloadURL);
+            }
+          );
+        }
+      );
+    });
+  };
+
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 11) {
+      setUploading(true);
+      setImageUploadError({ status: false, message: "" });
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(handleFileUpload(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((urls: any) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError({ status: false, message: "" });
+          setUploading(false);
+        })
+        .catch((error) => {
+          setImageUploadError({
+            status: false,
+            message: "Image upload failed (2mb max per image)",
+          });
+          setUploading(false);
+        });
+    } else if (files.length + formData.imageUrls.length > 10) {
+      setImageUploadError({
+        status: true,
+        message: "Image upload failed (maximum of 10 image upload is allowed)",
+      });
+      setUploading(false);
+    } else {
+      setImageUploadError({
+        status: true,
+        message: "Image upload failed (minimum of 1 image upload is allowed)",
+      });
+      setUploading(false);
+    }
+  };
+
+  const handleRemoveImage = (index: Number) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+
   return (
     <div className="flex flex-col -mt-20 px-4 max-w-4xl mx-auto box-border h-[calc(100vh-80px)] bg-background">
       <h1 className="text-3xl font-semibold text-center mt-32">
@@ -102,17 +201,68 @@ export default function CreateListing({ currentUser }: Props) {
               </span>
             </p>
             <div className="flex gap-4 mt-4">
-              <input
-                type="file"
-                id="images"
-                accept="image/*"
-                multiple
-                className="border w-9/12 border-gray-400 p-2 rounded-2xl"
-              />
-              <button className="border-secondary p-2 rounded-2xl bg-secondary font-semibold text-fontColor">
-                Upload
+              <div className="relative w-9/12">
+                <input
+                  type="file"
+                  id="images"
+                  accept="image/*"
+                  multiple
+                  className="hidden"
+                  onChange={handleChange}
+                />
+                <label
+                  htmlFor="images"
+                  className="border border-gray-200 p-2 rounded-2xl cursor-pointer bg-accent text-background text-center w-full block"
+                >
+                  Choose Files
+                </label>
+              </div>
+              <button
+                type="button"
+                disabled={uploading}
+                onClick={handleImageSubmit}
+                className="border-secondary p-2 rounded-2xl bg-secondary font-semibold text-fontColor"
+              >
+                {uploading ? "Uploading" : "Upload"}
               </button>
             </div>
+            <p className="text-red-500 text-center mt-3">
+              {imageUploadError.status && imageUploadError.message}
+            </p>
+            {formData.imageUrls.length > 0 &&
+              formData.imageUrls.map((url, index) => {
+                return (
+                  <div className="flex justify-around items-center mt-4 border rounded-2xl py-1">
+                    <img
+                      src={url}
+                      key={url}
+                      alt="image"
+                      className="size-20 object-cover rounded-lg border"
+                    />
+                    <span
+                      onClick={() => {
+                        handleRemoveImage(index);
+                      }}
+                      className="cursor-pointer border p-1 border-gray-200 rounded-md hover:bg-secondary hover:text-gray-500"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                );
+              })}
           </div>
         </div>
         <RippleButton className="p-3 bg-primary rounded-2xl text-background mt-10 w-full text-xl">
