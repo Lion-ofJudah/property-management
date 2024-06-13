c67aaeee3c457acf9ba3012c1d6dc46bc6522aa3
Metsakal Zeleke <MetsakalZ@gmail.com>
2024-06-13 19:10:22 +0300

    Create edit property api.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -21,3 +21,37 @@ export const deleteListing = async (req, res) => {
     .status(200)
     .json({ success: true, message: "Property deleted successfully." });
 };
+
+export const updateListing = async (req, res) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return errorHandler(res, 404, "Property not found!");
+  }
+
+  if (req.user._id !== listing.userRef) {
+    return errorHandler(res, 401, "Unauthorized action!");
+  }
+
+  const updatedListing = await Listing.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        address: req.body.address,
+        regularPrice: req.body.regularPrice,
+        discountPrice: req.body.discountPrice,
+        type: req.body.type,
+        imageUrls: imageUrls.push(req.body.imageUrls),
+        bedrooms: req.body.bedrooms,
+        bathrooms: req.body.bathrooms,
+        parking: req.body.parking,
+        furnished: req.body.furnished,
+      },
+    },
+    { new: true }
+  );
+
+  res.status(200).json(updatedListing);
+};
